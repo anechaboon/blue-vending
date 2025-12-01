f9f0a9002d0c0e4cdf74c91bac623683f9c17084
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2025-12-01 15:20:19 +0700

    adjust layout

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -12,7 +12,6 @@ interface Product {
   image: string | File;
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 const BASE_URL_IMAGE = process.env.NEXT_PUBLIC_IMAGE_URL;
 
 export default function Products({ onClickProduct }: { onClickProduct: (p: Product) => void }) {
@@ -35,12 +34,10 @@ export default function Products({ onClickProduct }: { onClickProduct: (p: Produ
             alt={product.title}
             className="w-full h-48 object-cover"
           />
-          <div className="p-4">
-            <h2 className="text-xl font-semibold mb-2 text-gray-800">
-              {product.title}
-            </h2>
-            <p className="text-gray-600">{product.price}</p>
-          </div>
+            <div className="flex flex-row justify-between mb-4 px-4 pt-3"> {/* row */}
+              <h2 className="text-2xl font-bold flex-grow basis-0" style={{ flex: '0 0 75%' }}>{product.title}</h2>
+              <h2 className="text-2xl font-bold flex-shrink-0 text-right" style={{ flex: '0 0 25%' }}>{product.price} ฿</h2>
+            </div>
         </div>
       ))}
     </div>
