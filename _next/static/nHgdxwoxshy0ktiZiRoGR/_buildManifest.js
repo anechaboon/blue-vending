7130ad701058e0c9ac413f58d7623c12c95e32fd
anechaboon <anechaboon@users.noreply.github.com>
2025-12-10 09:56:23 +0000

    deploy: 0c6f95c9bf678b576f6c9267877585bc012a90b4

--- _next/static/nHgdxwoxshy0ktiZiRoGR/_buildManifest.js
+++ _next/static/nHgdxwoxshy0ktiZiRoGR/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/blue-vending//_next/:path+",
+        "destination": "/blue-vending/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
